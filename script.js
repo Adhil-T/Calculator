0e8322109d655ace87df32022f81442b1ae52639
Adhil-T <adhil0236@gmail.com>
2023-11-20 15:31:54 +0530

    1.7

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 let scn = document.getElementById('screen');
+
+function addMod() {
+    let l = scn.innerHTML.length;
+    if(scn.innerHTML.charAt(l-1) == "+" || scn.innerHTML.charAt(l-1) == "-" || scn.innerHTML.charAt(l-1) == "*" || scn.innerHTML.charAt(l-1) == "/" || scn.innerHTML.charAt(l-1) == "%") {
+
+    }
+    else {
+        scn.innerHTML += "%";
+    }
+}
+
+function addPls() {
+    let l = scn.innerHTML.length;
+    if(scn.innerHTML.charAt(l-1) == "+" || scn.innerHTML.charAt(l-1) == "-" || scn.innerHTML.charAt(l-1) == "*" || scn.innerHTML.charAt(l-1) == "/" || scn.innerHTML.charAt(l-1) == "%") {
+
+    }
+    else {
+        scn.innerHTML += "+";
+    }
+}
+
+function addMin() {
+    let l = scn.innerHTML.length;
+    if(scn.innerHTML.charAt(l-1) == "+" || scn.innerHTML.charAt(l-1) == "-" || scn.innerHTML.charAt(l-1) == "*" || scn.innerHTML.charAt(l-1) == "/" || scn.innerHTML.charAt(l-1) == "%") {
+
+    }
+    else {
+        scn.innerHTML += "-";
+    }
+}
+
+function addMul() {
+    let l = scn.innerHTML.length;
+    if(scn.innerHTML.charAt(l-1) == "+" || scn.innerHTML.charAt(l-1) == "-" || scn.innerHTML.charAt(l-1) == "*" || scn.innerHTML.charAt(l-1) == "/" || scn.innerHTML.charAt(l-1) == "%") {
+
+    }
+    else {
+        scn.innerHTML += "*";
+    }
+}
+
+function addDiv() {
+    let l = scn.innerHTML.length;
+    if(scn.innerHTML.charAt(l-1) == "+" || scn.innerHTML.charAt(l-1) == "-" || scn.innerHTML.charAt(l-1) == "*" || scn.innerHTML.charAt(l-1) == "/" || scn.innerHTML.charAt(l-1) == "%") {
+
+    }
+    else {
+        scn.innerHTML += "/";
+    }
+}
